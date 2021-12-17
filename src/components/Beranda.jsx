4a945ccbb1282c891d21a4d1de410f9ea6c23131
daffajulistyo <daffajulistyo88@gmail.com>
2021-12-17 13:23:50 +0700

    Gass Commit

---
+++ src/components/Beranda.jsx
@@ -0,0 +1,10 @@
+function Beranda(){
+    return(
+        <div className="container mt-3 w-75">
+            <h1 className="text-center">Selamat Datang di Toko Buku Daffa</h1>
+            <div id="katalog"></div>
+        </div>
+    );
+}
+
+export default Beranda;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from '../assets/slack.png';
+
+function Navbar(){
+    return (
+        <div>
+            <nav className="navbar navbar-expand bg-dark">
+                <div className="container">
+                    <ul className="nav">
+                        <img src={logo} alt="" style={{ height: "40px" }}/>
+                        <li>
+                            <a href="/" className="nav-link">Beranda</a>
+                        </li>
+                        <li>
+                            <a href="/manajemen-buku" className="nav-link">Manajemen Buku</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
